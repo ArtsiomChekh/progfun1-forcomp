41afedfebc51d789e733a624bc24c677e48a24ac
artsiomchekh <qbartem@gmail.com>
2023-07-25 11:01:21 +0300

    Added the implementation of def combinations + test

--- src/main/scala/forcomp/Anagrams.scala
+++ src/main/scala/forcomp/Anagrams.scala
@@ -87,7 +87,12 @@ object Anagrams extends AnagramsInterface:
    * Note that the order of the occurrence list subsets does not matter -- the subsets
    * in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] =
+    List() :: (for {
+      (char, max) <- occurrences
+      count <- 1 to max
+      remained <- combinations(occurrences filter ((c, _) => c > char))
+    } yield List((char, count)) ++ remained)
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
